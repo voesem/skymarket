46575c43668d16131b86780463f823627e3173d4
voesem <voesem@gmail.com>
2023-12-21 19:34:01 +0300

    Добавлены маршруты для swagger и djoser

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -2,11 +2,34 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-# TODO здесь необходимо подклюючит нужные нам urls к проекту
+# Подключение нужных маршрутов к проекту
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1",
+        description="Your API description",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+
+    # path("", include("ads.urls", namespace="courses")),
+    # path("users/", include("users.urls", namespace="users")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
