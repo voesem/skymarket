ab59cbfba9e8e46e42eb01154440e3cd86482aad
voesem <voesem@gmail.com>
2023-12-24 11:47:12 +0300

    Созданы урлы для объявлений

--- app/ads/urls.py
+++ app/ads/urls.py
@@ -1,8 +1,16 @@
 from django.urls import include, path
 
-# TODO настройка роутов для модели
+from .apps import SalesConfig
+from .views import AdListAPIView, AdCreateAPIView, AdRetrieveAPIView, AdUpdateAPIView, AdDestroyAPIView
 
+# Маршруты для представлений моделей Ad и Comment
+
+app_name = SalesConfig.name
 
 urlpatterns = [
-
+    path('', AdListAPIView.as_view(), name='ads_list'),
+    path('create/', AdCreateAPIView.as_view(), name='create_ad'),
+    path('<int:pk>/', AdRetrieveAPIView.as_view(), name='get_ad'),
+    path('<int:pk>/update/', AdUpdateAPIView.as_view(), name='update_ad'),
+    path('<int:pk>/delete/', AdDestroyAPIView.as_view(), name='delete_ad'),
 ]
