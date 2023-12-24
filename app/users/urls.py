f5377813b0eb7fb1042edbf1debfcff8dabdd9d0
voesem <voesem@gmail.com>
2023-12-24 11:46:43 +0300

    Созданы урлы авторизации по токену

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,8 +1,17 @@
 from django.urls import include, path
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
-# TODO подключите UserViewSet из Djoser.views к нашим urls.py
-# TODO для этокого рекоммендуется использовать SimpleRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import MyTokenObtainPairView
+
+# Маршруты для представлений пользователей с использованием пакета Djoser
+users_router = SimpleRouter()
+
+users_router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
+    path('', include(users_router.urls)),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
