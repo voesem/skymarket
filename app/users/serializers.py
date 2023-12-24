273b399e330bfb4593f6582873e633e1831756aa
voesem <voesem@gmail.com>
2023-12-24 11:49:31 +0300

    Созданы сериализаторы регистрации и просмотра профиля пользователя

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,15 +1,43 @@
 from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 User = get_user_model()
-# TODO Здесь нам придется переопределить сериалайзер, который использует djoser
-# TODO для создания пользователя из за того, что у нас имеются нестандартные поля
+# Сериализаторы регистрации и просмотра профиля пользователя
 
 
 class UserRegistrationSerializer(BaseUserRegistrationSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'phone',
+        )
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
-    pass
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'phone',
+            'image',
+        )
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+
+        return token
