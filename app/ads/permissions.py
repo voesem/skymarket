471159ad5d3b8af4f03d2247a2dd7a169b5f5c41
voesem <voesem@gmail.com>
2024-01-08 11:44:09 +0300

    Настроены права доступа для авторизованных пользователей и администраторов

--- app/ads/permissions.py
+++ app/ads/permissions.py
@@ -1,1 +1,16 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.author:
+            return True
+
+        return False
+
+
+class IsAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.role.filter(name='admin').exists()
